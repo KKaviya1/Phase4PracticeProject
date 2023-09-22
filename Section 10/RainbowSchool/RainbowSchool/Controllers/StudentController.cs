be20df6d15243325da8a52da2b3a1cdae2d1b180
KKaviya1 <Kaviyagandhi136@gmail.com>
2023-09-22 20:30:49 +0530

    my commit

---
+++ Section 10/RainbowSchool/RainbowSchool/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RainbowSchool.Models;
+
+namespace RainbowSchool.Controllers
+{
+    public class StudentController : Controller
+    {
+        List<Student> student = new List<Student>()
+        {
+            new Student { Id = 1, Name = "Viya", Class = "9B", Age = 17, Address = "Mission Street"},
+            new Student { Id = 2, Name = "Prakash", Class = "7A", Age = 15, Address = "Pattanur"},
+            new Student { Id = 3, Name = "Praveen", Class = "8C", Age = 16, Address = "432 some where "},
+        };
+        public IActionResult Index()
+        {
+            return View(student);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Student());
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student model)
+        {
+            if (ModelState.IsValid)
+            {
+                student.Add(model);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+    }
+}
